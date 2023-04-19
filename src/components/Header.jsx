8363c0730cad56fa5c9f19aa843e99774fa38dac
Odohofre <odohofrebright@gmail.com>
2023-04-19 11:55:32 +0100

    style: remove unused white spaces

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -13,7 +13,7 @@ export default function Header() {
       </div>
       <div className="">
         <Link href="/">
-          <h2 className="text-2xl"><span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDB</span><span className=" text-xl hidden sm:inline">Clone</span></h2>
+          <h2 className="text-2xl"><span className="font-bold bg-amber-500 py-1 px-2 rounded-lg mr-1">IMDB</span><span className="text-xl hidden sm:inline">Clone</span></h2>
           
         </Link>
       </div>
